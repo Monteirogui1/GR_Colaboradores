e63f0883b71f41751c8b9b5cfdf3849a97bac090
Guilherme <guimonteiro8@gmail.com>
2026-03-20 15:52:25 -0300

    correções ativo

--- core/settings.py
+++ core/settings.py
@@ -144,9 +144,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Opcional: diretório para arquivos estáticos do projeto
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
